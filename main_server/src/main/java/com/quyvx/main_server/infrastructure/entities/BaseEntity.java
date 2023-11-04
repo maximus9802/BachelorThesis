5d7c45630a8dee0c8d7d85227993043b279ec7e6
maximus9802 <vuquy090802@gmail.com>
2023-11-04 14:50:54 +0700

    initialize database

---
+++ main_server/src/main/java/com/quyvx/main_server/infrastructure/entities/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.quyvx.main_server.infrastructure.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+public abstract class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT")
+    private Long id;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+}
